b562bdef1e436fb56b283e755cdbad586b272a0b
naveed60 <advmiannaveedali@gmail.com>
2024-02-06 15:54:40 +0500

    quiz logic updated  added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,6 +4,26 @@ import QUESTIONS from "../questuins.js";
 export default function Quiz(){
     const [userAnswers, setUsereAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
-        return ( <p>Active question</p>
-    )
+   function handleSelectAnswer(selectedAnswer){
+    setUsereAnswers((previousUserAnswers) => {
+      return [...previousUserAnswers, selectedAnswer];
+    });
+   };
+  return (
+ <>
+ <div id="quiz">
+    <div id="question">
+      <h1>{QUESTIONS[activeQuestionIndex].text}</h1>
+    </div>
+    <ul id="answers">
+      {QUESTIONS[activeQuestionIndex].answers.map((answer) =>(
+        <li key={answer} className="answer">
+          <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+        </li>
+      ))}
+
+    </ul>
+ </div>
+</>
+  )
 }
